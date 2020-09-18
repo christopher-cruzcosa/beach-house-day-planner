9689b88ec0aba5e000d8e2115ccd4e9983fcf54f
Chris C <christopher.cruzcosa@gmail.com>
2020-09-17 23:20:03 -0700

    Added core functionality to js

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # beach-house-day-planner
 A day planner with a focus on beach life.
+
+## Installation
+
+This web app is made up of an Index.html file and a script.js file, which controls the day planner functionality.
+
+The location is: https://christopher-cruzcosa.github.io/beach-house-day-planner/
+
+
+## Usage 
+
+![Screenshot](./assets/images/screenshot1.png)
+
+
+
+
+## Credits
+
+Created by: Christopher Cruzcosa
+
+
+
+## License
+
+Copyright (c) [2020] [Christopher Cruzcosa]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+
+
+$("#currentDay").text(moment().format('dddd, MMMM Do'));
+var currentHour = moment().format("k");
+var hour9Text = $("#hour-9").find("textArea");
+var hour10Text = $("#hour-10").find("textArea");
+var hour11Text = $("#hour-11").find("textArea");
+var hour12Text = $("#hour-12").find("textArea");
+var hour13Text = $("#hour-13").find("textArea");
+var hour14Text = $("#hour-14").find("textArea");
+var hour15Text = $("#hour-15").find("textArea");
+var hour16Text = $("#hour-16").find("textArea");
+var hour17Text = $("#hour-17").find("textArea");
+
+var hourEventList = ["","","","","","","","",""]
+
+
+if (JSON.parse(localStorage.getItem("savedHourEvents")) != null){
+  hourEventList = JSON.parse(localStorage.getItem("savedHourEvents"))
+};
+
+function setHourText() {
+    for (var i = 9; i < 18; i++) {
+        eval("hour" + i + "Text").val(hourEventList[(i-9)]);
+    };
+};
+
+function highlightHours () {
+    for (var i = 9; i < 18; i++) {
+        if (currentHour === i){
+            $(("#hour-" + i)).attr("class", "row time-block present");
+        };
+        if (currentHour > i){
+            $(("#hour-" + i)).attr("class", "row time-block past");
+        };
+        if (currentHour < i){
+            $(("#hour-" + i)).attr("class", "row time-block future");
+        };
+    }
+};
+
+
+setHourText();
+highlightHours();
+
+$(".saveBtn").on( "click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var textAreaToBeSaved = $(this).parent().find("textArea");
+    var num = $(this).parent().attr("id").slice(5);
+    var newEvent = textAreaToBeSaved.val();
+    hourEventList[(num-9)] = newEvent;
+    setHourText();
+    localStorage.setItem('savedHourEvents', JSON.stringify(hourEventList));
+    }
+);
+
